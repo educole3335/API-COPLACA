8d73ee0ca31bcefbb0b66b1e28987dcb62ac5ecd
educole3335 <usr3335@salesianos.net>
2026-02-19 09:58:02 +0000

    fix(User): Create class User and his parameters

---
+++ src/main/java/com/coplaca/apirest/entity/User.java
@@ -0,0 +1,33 @@
+package com.coplaca.apirest.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String address; // necesario para los cliente se inscriban en la wharehouse más cercana
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
