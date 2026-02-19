75eef4254eccd50b716e7bbad545bf6497bf8f1d
educole3335 <usr3335@salesianos.net>
2026-02-19 10:02:29 +0000

    fix(Peoduct): create class product and parameters

---
+++ src/main/java/com/coplaca/apirest/entity/Product.java
@@ -0,0 +1,29 @@
+package com.coplaca.apirest.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private String imageUrl;
+    private Double pricePerKg;
+    private Double stockKg;
+    private String status; // e.g., AVAILABLE, DISCONTINUED
+}
