c0b03d5f0359dec7f70b8c7719caa6af8b2f5d71
educole3335 <usr3335@salesianos.net>
2026-02-19 10:38:50 +0000

    fix(OrdenItem) create class orderitem and parameters

---
+++ src/main/java/com/coplaca/apirest/entity/OrderItem.java
@@ -0,0 +1,29 @@
+package com.coplaca.apirest.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "order_items")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Product product;
+
+    private Double quantityKg;
+    private Double price; // subtotal
+}
