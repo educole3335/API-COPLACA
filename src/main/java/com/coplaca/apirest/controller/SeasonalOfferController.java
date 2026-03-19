eee1579ae4167771f0371f807968533ade216734
educole3335 <usr3335@salesianos-lacuesta.net>
2026-03-19 11:45:18 +0000

    chore(api): centralize CORS handling in seasonal offer controller
    Removes controller-level CORS annotation to rely on centralized security/CORS configuration.

--- src/main/java/com/coplaca/apirest/controller/SeasonalOfferController.java
+++ src/main/java/com/coplaca/apirest/controller/SeasonalOfferController.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/offers")
-@CrossOrigin(origins = "*")
 public class SeasonalOfferController {
     private final SeasonalOfferService offerService;
 
