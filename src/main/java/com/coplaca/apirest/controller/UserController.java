23473f8f7405229d7fb9c2f70aefff98a7ce5be6
educole3335 <usr3335@salesianos-lacuesta.net>
2026-03-19 11:45:19 +0000

    chore(api): centralize CORS handling in user controller
    Removes controller-level CORS annotation to rely on centralized security/CORS configuration.

--- src/main/java/com/coplaca/apirest/controller/UserController.java
+++ src/main/java/com/coplaca/apirest/controller/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
-@CrossOrigin(origins = "*")
 public class UserController {
     
     private final UserService userService;
