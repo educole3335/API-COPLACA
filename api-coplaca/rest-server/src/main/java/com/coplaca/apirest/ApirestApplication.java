3dcb17d24db451a9cd4e89671a559d0ed90e02f8
educole3335 <usr3335@salesianos-lacuesta.net>
2026-03-29 12:41:43 +0100

    rest-server: explicitar escaneo de componentes
    Añade @ComponentScan para asegurar deteccion de beans en el paquete base com.coplaca.apirest.

--- api-coplaca/rest-server/src/main/java/com/coplaca/apirest/ApirestApplication.java
+++ api-coplaca/rest-server/src/main/java/com/coplaca/apirest/ApirestApplication.java
@@ -2,8 +2,10 @@ package com.coplaca.apirest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.coplaca.apirest")
 public class ApirestApplication {
 
 	public static void main(String[] args) {
