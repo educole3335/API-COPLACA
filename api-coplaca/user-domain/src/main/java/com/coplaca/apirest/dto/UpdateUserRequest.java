9eda161eb236b4eddb1365d4d995a337ce65a578
educole3335 <usr3335@salesianos-lacuesta.net>
2026-03-29 12:44:29 +0100

    user: extender DTO de actualizacion con email
    Agrega campo email en UpdateUserRequest para soportar cambio controlado de correo en perfil administrativo.

--- api-coplaca/user-domain/src/main/java/com/coplaca/apirest/dto/UpdateUserRequest.java
+++ api-coplaca/user-domain/src/main/java/com/coplaca/apirest/dto/UpdateUserRequest.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class UpdateUserRequest {
+    private String email;
     private String firstName;
     private String lastName;
     private String phoneNumber;
